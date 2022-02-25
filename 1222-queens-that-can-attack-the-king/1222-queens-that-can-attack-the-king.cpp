e514374e7b63a863085d8cb115c55e5bb1f5f09f
Prantik Mukherjee <champmaniac04@gmail.com>
2022-02-25 22:02:44 +0530

    Time: 14 ms (14.43%), Space: 11.1 MB (49.14%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool inside(int row,int col){
+        return 0<=row && row<8 && 0<=col && col<8;
+    }
+    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king){
+        vector<vector<bool>> taken(8,vector<bool>(8));
+        for(vector<int> queen:queens){
+            taken[queen[0]][queen[1]] =true;
+        }
+        vector<vector<int>> ans;
+        for(int d1=-1;d1<=1;d1++){
+            for(int d2=-1;d2<=1;d2++){
+                if(d1==0 && d2==0) continue;
+                int row = king[0];
+                int col = king[1];
+                do{
+                    row+=d1;
+                    col+=d2;
+                } while(inside(row,col) && !taken[row][col]);
+                if(inside(row,col)){
+                    ans.push_back({row,col});
+                }
+            }
+        }
+        return ans;
+    }
+};
