3ac1443d323a270d816a7e692ebfd92cd2081dcd
Prantik Mukherjee <champmaniac04@gmail.com>
2022-03-05 10:09:36 +0530

    Time: 6 ms (11.18%), Space: 6.6 MB (24.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,11 +1,11 @@
-// class Solution { // TC O(2^m*n) SC O(2^m*n) // Recursion
-// public:
-//     int uniquePaths(int m, int n,int i=0,int j=0) {
-//         if(i>=m || j>=n) return 0;
-//         if(i==m-1 || j==n-1) return 1;
-//         return uniquePaths(m,n,i+1,j)+uniquePaths(m,n,i,j+1);
-//     }
-// };
+class Solution { // TC O(2^m*n) SC O(2^m*n) // Recursion (TLE)
+public:
+    int uniquePaths(int m, int n,int i=0,int j=0) {
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 || j==n-1) return 1;
+        return uniquePaths(m,n,i+1,j)+uniquePaths(m,n,i,j+1);
+    }
+};
 
 class Solution { // TC O(m*n) SC O(m*n) // Dp Memoization
 public:
@@ -18,5 +18,18 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int> (n));
         return dfs(dp,0,0);
+    }
+};
+
+class Solution { // TC O(m*n) SC O(m*n) // Dp Tabulation
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int> (n,1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
