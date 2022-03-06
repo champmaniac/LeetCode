01529a4dbea10bf327a472f196dd1d28d7d9dae3
Prantik Mukherjee <champmaniac04@gmail.com>
2022-03-06 07:47:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.14%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        long res=1, mod =1e9+7;
+        for(int i=1;i<=n;i++)
+            res=res*(i*2-1)*i%mod;
+        return res;
+    }
+};
