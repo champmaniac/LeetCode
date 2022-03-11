6dcd5521cc7abcdb3c273f20bf2c2886f701746b
Prantik Mukherjee <champmaniac04@gmail.com>
2022-03-12 01:22:23 +0530

    Time: 25 ms (56.50%), Space: 7.5 MB (70.34%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lcs(string X, string Y,int m,int n){
+        int dp[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(X[i-1]==Y[j-1]) dp[i][j]= 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        return dp[m][n];
+    }
+    int minDistance(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        return m-lcs(s1,s2,m,n)+n-lcs(s1,s2,m,n) ;
+    }
+};
