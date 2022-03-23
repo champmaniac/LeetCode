e42d8fb924af5b5a68ac8a1206d0d875203cb059
Prantik Mukherjee <champmaniac04@gmail.com>
2022-03-23 23:27:21 +0530

    Added solution - LeetHub

---
+++ 2. Left most and right most index - GFG/2.-left-most-and-right-most-index.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        // code here
+        if(binary_search(v.begin(),v.end(),x)){
+            return {lower_bound(v.begin(),v.end(),x)-v.begin(),upper_bound(v.begin(),v.end(),x)-v.begin()-1};
+        }
+        else
+            return {-1,-1};
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        vector<long long>v;
+        cin>>n;
+        for(long long i=0;i<n;i++)
+        {
+            cin>>k;
+            v.push_back(k);
+        }
+        long long x;
+        cin>>x;
+        Solution obj;
+        pair<long,long> pair = obj.indexes(v, x);
+        
+        if(pair.first==pair.second and pair.first==-1)
+        cout<< -1 <<endl;
+        else
+        cout<<pair.first<<" "<<pair.second<<endl;
+
+    }
+	return 0;
+}
+  // } Driver Code Ends
