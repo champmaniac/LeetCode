d80a815f864299baef54d3b391c3398cc6fb2900
Prantik Mukherjee <champmaniac04@gmail.com>
2022-03-24 10:51:09 +0530

    Added solution - LeetHub

---
+++ 1. Key Pair - GFG/1.-key-pair.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    unordered_map<int,int> mp;
+	    for(int i=0;i<n;i++){
+	        int a = arr[i];
+	        int b = x-a;
+	        if(mp.find(b)!=mp.end()){
+	            return true;
+	        }
+	        mp[arr[i]]=i;
+	    }
+	    return false;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
