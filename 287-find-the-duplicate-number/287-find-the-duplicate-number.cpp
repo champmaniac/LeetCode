cc7fd8abc14b06ebaa51a514d60d88dec4cbefa6
Prantik Mukherjee <champmaniac04@gmail.com>
2022-03-31 20:41:28 +0530

    Time: 125 ms (79.87%), Space: 61.2 MB (61.31%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,24 +1,24 @@
 class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        int i=1,n=nums.size();
-        while(i<n){
-            if(nums[i]!=i+1){
-                int correct = nums[i]-1;
-                if(nums[i]!=nums[correct])
-                    swap(nums,i,correct);
+    public:
+        int findDuplicate(vector<int>& nums) {
+            int i=0,n=nums.size(); // i is the start of the range and n is the end of the range
+            while(i<n){ // i value is less than n value
+                if(nums[i]!=i+1){ // if the value is not equal to the index value
+                    int correct = nums[i]-1; // correct is the value that is supposed to be in the index
+                    if(nums[i]!=nums[correct]) // if the value is not equal to the value in the index
+                        swap(nums,i,correct); // swap the values
+                    else
+                        return nums[i]; // return the value
+                }
                 else
-                    return nums[i];
+                    i++; // increment i
             }
-            else
-                i++;
+            return -1; // if no duplicate is found
         }
-        return -1;
-    }
-    
-    void swap(vector<int> &nums, int first, int second){
-			int temp =nums[first];
-			nums[first] = nums[second];
-			nums[second] =temp;
-	}
-};
+        
+        void swap(vector<int> &nums, int first, int second){ // swap function
+                int temp =nums[first];
+                nums[first] = nums[second];
+                nums[second] =temp;
+        }
+    };
