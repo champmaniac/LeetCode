1cb3ffbc6e09ea5253d553af120ac153ebabe45b
Prantik Mukherjee <champmaniac04@gmail.com>
2022-04-04 21:27:51 +0530

    Time: 184 ms (38.02%), Space: 61.2 MB (91.63%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,24 +1,11 @@
 class Solution {
-    public:
-        int findDuplicate(vector<int>& nums) {
-            int i=0,n=nums.size(); // i is the start of the range and n is the end of the range
-            while(i<n){ // i value is less than n value
-                if(nums[i]!=i+1){ // if the value is not equal to the index value
-                    int correct = nums[i]-1; // correct is the value that is supposed to be in the index
-                    if(nums[i]!=nums[correct]) // if the value is not equal to the value in the index
-                        swap(nums,i,correct); // swap the values
-                    else
-                        return nums[i]; // return the value
-                }
-                else
-                    i++; // increment i
-            }
-            return -1; // if no duplicate is found
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums),end(nums));
+        for(int i=0;i<n;i++){
+            if(nums[i]==nums[i+1]) return nums[i];
         }
-        
-        void swap(vector<int> &nums, int first, int second){ // swap function
-                int temp =nums[first];
-                nums[first] = nums[second];
-                nums[second] =temp;
-        }
-    };
+        return -1;
+    }
+};
