906c19e3f5cf032807ad0e6d31f81b477c206a1e
Prantik Mukherjee <champmaniac04@gmail.com>
2022-04-05 19:03:28 +0530

    Time: 140 ms (95.60%), Space: 78.5 MB (55.79%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>heights){
-        int n = heights.size();
+    int largestRectangleArea(vector<int>histo){  // TC O(N) (single pass)
+        int n = histo.size();
+        int maxA=0;
         stack<int> st;
-        int leftSmall[n], rightSmall[n];
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-            if(st.empty()) leftSmall[i]=0;
-            else leftSmall[i]=st.top()+1;
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || histo[st.top()]>=histo[i])){
+                int height = histo[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i-st.top()-1;
+                maxA = max(maxA,width*height);
+            }
             st.push(i);
         }
-        while(!st.empty()) st.pop(); // clearing the stack to be reused
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-            if(st.empty()) rightSmall[i]=n-1;
-            else rightSmall[i]=st.top()-1;
-            st.push(i);
-        }
-        int maxi =0;
-        for(int i=0;i<n;i++){
-            maxi = max(maxi,heights[i]*(rightSmall[i]-leftSmall[i]+1));
-        }
-        return maxi;
+        return maxA;
     }
+    
+    
+    // int largestRectangleArea(vector<int>heights){ // TC O(4N) SC O(3N)
+    //     int n = heights.size();
+    //     stack<int> st;
+    //     int leftSmall[n], rightSmall[n];
+    //     for(int i=0;i<n;i++){
+    //         while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+    //         if(st.empty()) leftSmall[i]=0;
+    //         else leftSmall[i]=st.top()+1;
+    //         st.push(i);
+    //     }
+    //     while(!st.empty()) st.pop(); // clearing the stack to be reused
+    //     for(int i=n-1;i>=0;i--){
+    //         while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+    //         if(st.empty()) rightSmall[i]=n-1;
+    //         else rightSmall[i]=st.top()-1;
+    //         st.push(i);
+    //     }
+    //     int maxi =0;
+    //     for(int i=0;i<n;i++){
+    //         maxi = max(maxi,heights[i]*(rightSmall[i]-leftSmall[i]+1));
+    //     }
+    //     return maxi;
+    // }
     
     
     // int largestRectangleArea(vector<int>& heights) { // TC O(N^2)
