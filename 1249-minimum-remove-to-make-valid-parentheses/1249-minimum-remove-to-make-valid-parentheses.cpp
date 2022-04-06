b21d7a01fb1d6b21fb2f9f1700cf00ea3a770bb6
Prantik Mukherjee <champmaniac04@gmail.com>
2022-04-06 09:38:06 +0530

    Time: 21 ms (83.02%), Space: 11.5 MB (51.19%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         stack<int> st;
-        for(int i=0;i<s.size();++i){
-            if(s[i] =='(')
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')
                 st.push(i);
             else if(s[i]==')'){
-                if(st.empty())
+                if(st.empty()) 
                     s[i]='*';
                 else
                     st.pop();
-            }        
+            }
         }
-        while(!st.empty())
-        {
+        
+        while(!st.empty()){
             s[st.top()]='*';
             st.pop();
         }
         string ans="";
-        for(int i=0;i<s.size();++i){
-            if(s[i]!='*')
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='*'){
                 ans.push_back(s[i]);
+            }
         }
         return ans;
     }
