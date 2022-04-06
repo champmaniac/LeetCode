68c78e760f9776ebea106ed4426e8c02253a4244
Prantik Mukherjee <champmaniac04@gmail.com>
2022-04-06 21:10:54 +0530

    Time: 4 ms (96.11%), Space: 6.9 MB (90.56%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxans=INT_MIN;
+        int store[256]={0}; //array to store the occurences of all the characters
+        int l=0; // left pointer
+        int r=0; // right pointer
+        int ans=0; // intialising the length as 0
         int n = s.size();
-        if(n==0) return 0;
-        unordered_set<int> st;
-        int i=0;
-        for(int j=0;j<n;j++){
-            if(st.find(s[j])!=st.end()){
-                while(i<j && st.find(s[j])!=st.end()){
-                    st.erase(s[i]);
-                    i++;
-                }
+        while(r<n){ //iterate over the string till the right pointer reaches the end of the string 
+            store[s[r]]++; // increment the count of the character present in the right pointer
+            while(store[s[r]]>1){ // if occurance is >1 it means the character is repeated
+                store[s[l]]--; // reduce the occurance of left pointer as it might be present ahead also in the string  
+                l++; // increment the left pointer
             }
-            st.insert(s[j]);
-            maxans = max(maxans,j-i+1);
+            ans = max(ans,r-l+1); // as index starts from 0, so the ans = (right pointer-left pointer+1)
+            r++; // increment the right pointer
         }
-        return maxans;
+        return ans;
     }
 };
