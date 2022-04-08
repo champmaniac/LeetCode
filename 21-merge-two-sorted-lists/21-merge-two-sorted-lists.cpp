3efd05904ab7d27dde330e9c33975a38a9726e03
Prantik Mukherjee <champmaniac04@gmail.com>
2022-04-08 20:57:04 +0530

    Time: 4 ms (94.30%), Space: 14.7 MB (82.05%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,54 +8,21 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-class Solution{ // Iterative TC O(N+M) SC O(N+M)
+class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+    ListNode* merge(ListNode* l1, ListNode* l2){
         if(l1==NULL) return l2;
         if(l2==NULL) return l1;
-        ListNode * ans=new ListNode(-1);
-        ListNode *tail=ans;
-        while(l1!=NULL && l2!=NULL)
-        {
-            if(l1->val <l2->val)
-            {
-                tail->next=l1;
-                tail=l1;
-                l1=l1->next;
-            }
-            else
-            {
-                tail->next=l2;
-                tail=l2;
-                l2=l2->next;
-            }
+        if(l1->val<l2->val){
+            l1->next = merge(l1->next,l2);
+            return l1;
         }
-        if(l1==NULL) 
-            tail->next =l2;
-        else 
-            tail->next =l1;
-        return ans->next;
+        else{
+            l2->next = merge(l1,l2->next);
+            return l2;
+        }
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        return merge(list1,list2);
     }
 };
-// class Solution { // Recursive TC O(N+M) SC O(N+M)
-// public:
-//     ListNode* merge(ListNode* l1,ListNode* l2)
-//     {
-//         if(l1==NULL) return l2;
-//         if(l2==NULL) return l1;
-//         if(l1->val<l2->val)
-//         {
-//             l1->next = merge(l1->next,l2);
-//             return l1;
-//         }
-//         else
-//         {
-//             l2->next = merge(l1,l2->next);
-//             return l2;
-//         }
-//     }
-//     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-//         return merge(l1,l2);
-//     }
-// };
