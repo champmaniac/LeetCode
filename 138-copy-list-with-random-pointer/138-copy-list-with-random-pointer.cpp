45bbc0ca249ca46d797db58d983ee24c722d9f3a
Prantik Mukherjee <champmaniac04@gmail.com>
2022-04-10 17:36:37 +0530

    Time: 23 ms (6.23%), Space: 11.2 MB (63.26%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -1,21 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        map<Node*,Node*>mp; 
-        int i=0;
-        Node* ptr = head; // ptr is the pointer of the original list
-        while(ptr) // loop through the original list
-        {
-            mp[ptr] = new Node(ptr->val); // create a new node with the same value
-            ptr = ptr->next; // move to the next node
+        unordered_map<Node*,Node*>mp;
+        Node* temp = head;
+        while(temp!=NULL){
+            Node* copy = new Node(temp->val);
+            mp[temp]=copy;
+            temp=temp->next;
         }
-        ptr=head; // reset ptr to the head of the original list
-        while(ptr) // loop through the original list
-        {
-            mp[ptr]->next = mp[ptr->next]; // set the next pointer of the new node to the next node of the original node
-            mp[ptr]->random = mp[ptr->random]; // set the random pointer of the new node to the random node of the original node
-            ptr = ptr->next; // move to the next node
+        temp = head;
+        while(temp!=NULL){
+            mp[temp]->next = mp[temp->next];
+            mp[temp]->random = mp[temp->random];
+            temp=temp->next;
         }
-        return mp[head]; // return the head of the new list
+        return mp[head];
     }
 };
