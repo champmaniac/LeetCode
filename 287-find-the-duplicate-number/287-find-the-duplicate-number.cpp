fc177d337f57bd64301809b7d61aa917a961c4f7
Prantik Mukherjee <champmaniac04@gmail.com>
2022-04-21 08:34:18 +0530

    Time: 121 ms (81.71%), Space: 61.3 MB (26.48%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums){
-        int n = nums.size();
-        vector<int> freq(n+1,0);
-        for(int i=0;i<n;i++){
-            if(freq[nums[i]]==0) freq[nums[i]]+=1;
-            else return nums[i];
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
-        return 0;
+        while(slow!=fast);
+        fast = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
-    
-    // int findDuplicate(vector<int>& nums) { // TC O(NlogN)
-    //     int n = nums.size();
-    //     sort(begin(nums),end(nums));
-    //     for(int i=0;i<n;i++){
-    //         if(nums[i]==nums[i+1]) return nums[i];
-    //     }
-    //     return -1;
-    // }
 };
