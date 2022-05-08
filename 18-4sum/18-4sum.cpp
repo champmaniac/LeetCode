2291693960766f5e8c813cc072db91223e127904
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-08 12:19:54 +0530

    Time: 47 ms (87.69%), Space: 13.3 MB (50.62%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
         int n = nums.size();
+        vector<vector<int>> res;
         if(n==0) return res;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            int target_3=target-nums[i];
+            int t_3 = target-nums[i];
             for(int j=i+1;j<n;j++){
-                int target_2 = target_3-nums[j];
+                int t_2 = t_3-nums[j];
                 int start = j+1;
                 int end = n-1;
                 while(start<end){
                     int two_sum = nums[start]+nums[end];
-                    if(two_sum<target_2) start++;
-                    else if(two_sum>target_2) end--;
+                    if(t_2>two_sum) start++;
+                    else if(t_2<two_sum) end--;
                     else{
-                        vector<int> quadruplet(4, 0);
-                        quadruplet[0] = nums[i];
-                        quadruplet[1] = nums[j];
-                        quadruplet[2] = nums[start];
-                        quadruplet[3] = nums[end];
-                        res.push_back(quadruplet);
-                        while (start < end && nums[start] == quadruplet[2]) ++start;
-                        while (start < end && nums[end] == quadruplet[2]) --end;
+                        vector<int> quad(4,0);
+                        quad[0] = nums[i];
+                        quad[1] = nums[j];
+                        quad[2] = nums[start];
+                        quad[3] = nums[end];
+                        res.push_back(quad);
+                        while(start<end && nums[start]==quad[2]) ++start;
+                        while(start<end && nums[end] == quad[2]) --end;
                     }
                 }
-                while(j+1<n && nums[j + 1] == nums[j]) ++j;
+                while(j+1<n && nums[j+1]==nums[j]) j++;
             }
-            while(i+1<n && nums[i + 1] == nums[i]) ++i;
+            while(i+1<n && nums[i+1]==nums[i]) i++;
         }
         return res;
     }
