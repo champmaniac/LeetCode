d485fdc36abb018a7acbcd6065db9f5d9e0c05b3
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-15 16:19:47 +0530

    Time: 212 ms (39.01%), Space: 48 MB (34.82%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,33 +1,45 @@
 class Solution {
 public:
+    int dx[4]= {1,0,-1,0};
+    int dy[4]= {0,1,0,-1};
     int shortestPath(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, -1));
+        vector<vector<int>> vis(m, vector<int>(n, -1));
         
         queue<vector<int>> q;
         // x, y, currentLength, remaining k
         q.push({0,0,0,k});
-        
         while(!q.empty()){
-            auto t = q.front();
-            q.pop();
-            int x=t[0], y= t[1];
-            
-            if(x<0 || y<0 || x>=m || y>=n) continue;
-            
-            if(x==m-1 && y==n-1) return t[2];
-            
-            if(grid[x][y]==1){
-                if(t[3]>0) t[3]--;
-                else continue;
+            int sz = q.size();
+            while(sz-- >0){
+                int x = q.front()[0];
+                int y = q.front()[1];
+                int step = q.front()[2];
+                int newK = q.front()[3];
+                
+                q.pop();
+                // reached the destination
+                if(x==m-1 && y==n-1) return step;
+                // boundary check
+                if(x<0 || y<0 || x>=m || y>=n) continue;
+                // the newK is less than what the k value that the visited array has already seen
+                // i.e less superpower(newK) has come then just continue
+                if(vis[x][y]!=-1 && vis[x][y]>=newK) continue;
+                
+                if(grid[x][y]==1){
+                    if(newK>0)
+                        newK--;
+                    else
+                        continue;
+                }
+                vis[x][y] = newK;
+                for(int i=0;i<4;i++){
+                    int x1 = x+dx[i];
+                    int y1 = y+dy[i];
+                    q.push({x1,y1,step+1,newK});
+                }
             }
-            if(visited[x][y]!=-1 && visited[x][y]>=t[3]) continue;
-            visited[x][y]=t[3];
-            q.push({x+1, y, t[2]+1, t[3]});
-            q.push({x-1, y, t[2]+1, t[3]});
-            q.push({x, y+1, t[2]+1, t[3]});
-            q.push({x, y-1, t[2]+1, t[3]});
         }
         return -1;
     }
