eba4e3f08b4231048473f737acccc8f5493e8a51
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-17 11:33:42 +0530

    Time: 274 ms (19.07%), Space: 43.6 MB (27.39%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int src) {
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>> adj(105);
         for(auto t:times){
             adj[t[0]].push_back({t[1],t[2]});
@@ -8,21 +8,24 @@ public:
         
         vector<int> dist(n+1,INT_MAX);
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        dist[src]=0;
-        pq.push({0,src});
+        dist[k]=0;
+        pq.push({0,k});
+        
         while(!pq.empty()){
             int vertex = pq.top().second;
             pq.pop();
-            for(auto x:adj[vertex]){
+            
+            for(auto x: adj[vertex]){
                 int next_pos = x.first;
-                int next_dist = x.second;
-                if(dist[vertex]+next_dist<dist[next_pos]){
-                    dist[next_pos] = dist[vertex]+next_dist;
+                int next_dis = x.second;
+                
+                if(dist[vertex]+next_dis<dist[next_pos]){
+                    dist[next_pos] = dist[vertex]+next_dis;
                     pq.push({dist[next_pos],next_pos});
                 }
             }
         }
-        int ans = *max_element(dist.begin()+1,dist.end());
-        return ans==INT_MAX?-1:ans;
+        int ans = *max_element(dist.begin()+1, dist.end());
+        return ans==INT_MAX?-1: ans;
     }
 };
