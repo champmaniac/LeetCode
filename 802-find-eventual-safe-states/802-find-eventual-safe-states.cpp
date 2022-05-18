34ad74a9e8090fbd6356ff1addfce14776629d11
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-18 19:09:44 +0530

    Time: 315 ms (21.78%), Space: 61.7 MB (21.10%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &graph, int V, vector<int> &dp){
-        if(dp[V]) return dp[V]==1;
-        dp[V]=-1;
-        for(auto it=graph[V].begin();it!=graph[V].end();it++){
-            if(!dfs(graph,*it,dp)) return false;
-        }
-        dp[V]=1;
-        return true;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> res;
-        vector<int> dp(V,0);
+        int n =graph.size();
+        vector<int> indegree(n,0);
+        vector<vector<int>> adj(n);
+        vector<int> safeNodes;
         
-        for(int i=0;i<V;i++){
-            if(dfs(graph,i,dp))
-                res.push_back(i);
+        int i=0;
+        for(auto &edges:graph){
+            for(auto &edge:edges){
+                adj[edge].push_back(i);
+                indegree[i]++;
+            }
+            i++;
         }
-        return res;
+        
+        //topo sort
+        
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safeNodes.push_back(node);
+            
+            for(auto &edges:adj[node]){
+                if(--indegree[edges]==0)q.push(edges);
+            }
+        }
+        sort(begin(safeNodes),end(safeNodes));
+        return safeNodes;
     }
 };
