f1faf3fb6c051210f675614c22f0ddbb81fb75bb
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-21 09:29:41 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,59 +7,33 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	
-	vector<int> topoSort(int V, vector<int> adj[]) {
-	    stack<int> st;
-	    vector<int> vis(V,0);
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    
+	    // code here
+	    queue<int> q;
+	    vector<int> inDegree(V,0);
 	    for(int i=0;i<V;i++){
-	        if(vis[i]==0){
-	            dfs(i,st,adj,vis);
+	        for(auto &e:adj[i]){
+	            inDegree[e]++;
 	        }
 	    }
+	    
+	    for(int i=0;i<V;i++){
+	        if(inDegree[i]==0) q.push(i);
+	    }
+	    
 	    vector<int> topo;
-	    while(!st.empty()){
-	        topo.push_back(st.top());
-	        st.pop();
+	    while(!q.empty()){
+	        int top = q.front();
+	        topo.push_back(top);
+	        q.pop();
+	        for(auto v:adj[top]){
+	            if(--inDegree[v]==0) q.push(v);
+	        }
 	    }
 	    return topo;
 	}
-	void dfs(int node, stack<int> &st, vector<int> adj[], vector<int> &vis){
-	    vis[node] = 1;
-	    for(auto it: adj[node]){
-	        if(!vis[it]){
-	            dfs(it,st,adj,vis);
-	        }
-	    }
-	    st.push(node);
-	}
-	
-	
-// 	vector<int> topoSort(int V, vector<int> adj[]) 
-// 	{
-// 	    // code here
-// 	    queue<int> q;
-// 	    vector<int> indegree(V,0);
-// 	    for(int i=0;i<V;i++){
-// 	        for(auto it:adj[i])
-// 	            indegree[it]++;
-// 	    }
-// 	    for(int i=0;i<V;i++){
-// 	        if(!indegree[i]) q.push(i);
-// 	    }
-// 	    vector<int> topo;
-// 	    while(!q.empty()){
-// 	        int node = q.front();
-// 	        q.pop();
-// 	        topo.push_back(node);
-// 	        for(auto it:adj[node]){
-// 	            indegree[it]--;
-// 	            if(!indegree[it]){
-// 	                q.push(it);
-// 	            }
-// 	        }
-// 	    }
-// 	    return topo;
-// 	}
 };
 
 // { Driver Code Starts.
