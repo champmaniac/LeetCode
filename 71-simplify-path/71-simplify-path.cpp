33b5af2738efad3bbca1b4f14346608a53829982
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-23 18:48:23 +0530

    Time: 14 ms (19.71%), Space: 10.5 MB (38.56%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int n = path.size(); // n is the length of the path
-        string res,temp; // res is the result string and temp is the temp string
-        vector<string> st; // st is the stack 
-        stringstream ss(path); // ss is the stringstream to read the path 
-        while(getline(ss,temp,'/')){ // getline(ss,temp,'/') is to get the string in the path
-            if(temp=="" || temp ==".") continue; // if temp is empty or temp is ".", continue
-            if(!st.empty() && temp=="..") // if temp is ".." and st is not empty
-                st.pop_back(); // pop the top of st 
-            else if(temp!="..") // if temp is not ".."
-                st.push_back(temp); // push the temp into st
+        stack<string> st;
+        string res ="";
+        for(int i=0;i<path.size();i++){
+            if(path[i]=='/') continue;
+            string temp="";
+            while(i<path.size() && path[i]!='/'){
+                temp+=path[i];
+                ++i;
+            }
+            if(temp==".") continue;
+            else if(temp==".."){
+                if(!st.empty()) st.pop();
+            }
+            else st.push(temp);
         }
-        for(auto s:st){ // for each string in st
-            res+="/"+s; // add "/" and s into res
+        while(!st.empty()){
+            res = "/"+st.top()+res;
+            st.pop();
         }
-        return st.empty()? "/":res; // if st is empty, return "/" else return res
-        
+        if(res.size()==0) return "/";
+        return res;
     }
 };
