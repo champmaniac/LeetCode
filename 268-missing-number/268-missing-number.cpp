7988ee012c6e3558038538a382b34803089959c0
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-28 05:52:55 +0530

    Time: 25 ms (59.12%), Space: 17.9 MB (65.37%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int result = nums.size();
+        int i=0;
+        for(int num:nums){
+            result ^= num;
+            result ^= i;
+            i++;
+        }
+        return result;
+    }
+};
