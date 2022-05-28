573dbea14d976b56994dddb0675cff723f726292
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-28 10:42:53 +0530

    Time: 8 ms (83.79%), Space: 14.6 MB (8.61%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor){
-        vector<vector<int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
-		int m=image.size();
-        int n=image[0].size();
-        int oldcolor=image[sr][sc];
-        
-        if(image[sr][sc]==newColor) return image;
-        
+    bool isValid(int i,int j,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n) return false;
+        return true;
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int m = image.size();
+        int n = image[0].size();
+        int oldColor = image[sr][sc];
+        if(oldColor==newColor) return image;
         queue<pair<int,int>> q;
+        vector<vector<int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
         q.push({sr,sc});
         image[sr][sc] = newColor;
-        
         while(!q.empty()){
             auto top = q.front();
             q.pop();
-            
             for(auto move:dir){
-                int x = top.first + move[0];
-                int y = top.second + move[1];
+                int x = top.first+move[0];
+                int y = top.second+move[1];
                 
-                if(isValid(x,y,m,n) && image[x][y]==oldcolor){
+                if(isValid(x,y,m,n) && image[x][y]==oldColor){
                     q.push({x,y});
                     image[x][y] = newColor;
                 }
             }
         }
         return image;
-    }
-    
-    bool isValid(int i,int j,int m,int n){
-        if(i<0 || j<0 || i>=m || j>=n) return false;
-        return true;
     }
 };
