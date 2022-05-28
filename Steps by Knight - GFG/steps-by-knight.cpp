1fc71b234df0dda4bba6245489290d4fee603ba0
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-28 09:33:30 +0530

    Added solution - LeetHub

--- Steps by Knight - GFG/steps-by-knight.cpp
+++ Steps by Knight - GFG/steps-by-knight.cpp
@@ -7,30 +7,33 @@ class Solution
 {
     public:
     //Function to find out minimum steps Knight needs to reach target position.
-	int minStepToReachTarget(vector<int>&k,vector<int>&t,int n)
+    bool isvalid(int i,int j,int n){
+        if(i>0 && j>0 && i<=n && j<=n) return true;
+        return false;
+    }
+	int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int N)
 	{
 	    // Code here
-	    int m[8][2]={{-2,-1},{-2,1},{-1,2},{-1,-2},{1,2},{1,-2},{2,-1},{2,1}};
+	    int dir[8][2]={{-2,-1},{-2,1},{-1,2},{-1,-2},{1,2},{1,-2},{2,-1},{2,1}};
 	    queue<pair<int,int>> q;
-	    q.push({k[0],k[1]});
-	    vector<vector<int>> vis(n+1,vector<int>(n+1,0));
-	    vis[k[0]][k[1]] =1;
+	    q.push({KnightPos[0],KnightPos[1]});
+	    vector<vector<int>> vis(N+1,vector<int> (N+1,0));
+	    vis[KnightPos[0]][KnightPos[1]]=1;
 	    int ans=0;
 	    while(!q.empty()){
-	        int size = q.size();
-	        while(size-- >0){
-	            pair<int,int> p = q.front();
+	        int sz = q.size();
+	        while(sz--){
+	            auto top = q.front();
 	            q.pop();
-	            int x = p.first;
-	            int y = p.second;
-	            if(x==t[0] && y==t[1]) 
-	                return ans;
+	            int x = top.first;
+	            int y = top.second;
+	            if(x==TargetPos[0] && y==TargetPos[1]) return ans;
 	            for(int i=0;i<8;i++){
-	                int cx = x+m[i][0];
-	                int cy = y+m[i][1];
-	                if(cx>0 && cy>0 && cx<=n && cy<=n && !vis[cx][cy]){
-	                    q.push({cx,cy});
-	                    vis[cx][cy]=1;
+	                int di = x+dir[i][0];
+	                int dj = y+dir[i][1];
+	                if(isvalid(di,dj,N) && !vis[di][dj]){
+	                    q.push({di,dj});
+	                    vis[di][dj]=1;
 	                }
 	            }
 	        }
