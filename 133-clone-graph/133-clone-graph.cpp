a1655ce5641a7fb5d6ade12a73fb9770e7b89d19
Prantik Mukherjee <champmaniac04@gmail.com>
2022-05-31 20:35:38 +0530

    Time: 12 ms (26.97%), Space: 8.5 MB (85.08%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -19,17 +19,42 @@ public:
 };
 */
 
-class Solution { // DFS APPROACH
+class Solution { // BFS APPROACH
 public:
     unordered_map<Node*,Node*> mp;
     Node* cloneGraph(Node* node) {
         if(node==NULL) return NULL;
-        if(mp.find(node)==mp.end()){ // node not present in map
-            mp[node]= new Node(node->val,{}); // making a copy
-            for(auto adj:node->neighbors){ // travel to it's adjacent
-                mp[node]->neighbors.push_back(cloneGraph(adj)); // add the copy
+        queue<Node*> q;
+        Node* first = new Node(node->val,{}); 
+        mp[node]=first;
+        q.push(node);
+        while(!q.empty()){
+            auto cur = q.front();
+            q.pop();
+            
+            for(auto adj:cur->neighbors){
+                if(mp.find(adj)==mp.end()){ // node not present in map
+                    mp[adj] = new Node(adj->val,{});
+                    q.push(adj);
+                }
+                mp[cur]->neighbors.push_back(mp[adj]);
             }
         }
-        return mp[node]; // return the deep copy(clone) of the graph
+        return mp[node];
     }
 };
+
+// class Solution { // DFS APPROACH
+// public:
+//     unordered_map<Node*,Node*> mp;
+//     Node* cloneGraph(Node* node) {
+//         if(node==NULL) return NULL;
+//         if(mp.find(node)==mp.end()){ // node not present in map
+//             mp[node]= new Node(node->val,{}); // making a copy
+//             for(auto adj:node->neighbors){ // travel to it's adjacent
+//                 mp[node]->neighbors.push_back(cloneGraph(adj)); // add the copy
+//             }
+//         }
+//         return mp[node]; // return the deep copy(clone) of the graph
+//     }
+// };
