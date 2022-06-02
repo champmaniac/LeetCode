92cc2fcd4e85878905d92e8056501d130626b333
Prantik Mukherjee <champmaniac04@gmail.com>
2022-06-02 09:14:46 +0530

    Time: 34 ms (38.33%), Space: 14.2 MB (46.87%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool isCycle(int node, vector<int> &vis, vector<int> adj[]){
-        if(vis[node]==1) return true;
-        if(!vis[node]){
-            vis[node]=1;
-            for(auto edges:adj[node]){
-                if(isCycle(edges,vis,adj)) return true;
-            }
-        }
-        vis[node]=2;
-        return false;
+    bool isCycle(int ind, vector<int> &vis, vector<int> adj[]){
+       if(vis[ind]==1) return true;
+       if(!vis[ind]){
+           vis[ind]=1;
+           for(auto edge:adj[ind]){
+               if(isCycle(edge,vis,adj)) return true;
+           }
+       }
+       vis[ind]=2;
+       return false;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n];
         for(auto edges:prerequisites){
             adj[edges[1]].push_back(edges[0]);
         }
-        vector<int> vis(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            if(isCycle(i,vis,adj))
-                return false;
+        vector<int> vis(n,0);
+        for(int i=0;i<n;i++){
+            if(isCycle(i,vis,adj)) return false;
         }
         return true;
     }
