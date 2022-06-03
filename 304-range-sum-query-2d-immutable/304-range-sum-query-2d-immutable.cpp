ffa6a348c8fa8a830b2d516d82a245555af07f58
Prantik Mukherjee <champmaniac04@gmail.com>
2022-06-03 06:00:40 +0530

    Time: 646 ms (40.94%), Space: 148.1 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,23 +1,19 @@
 class NumMatrix {
 public:
+    vector<vector<int>> sum;
     NumMatrix(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        accu = matrix;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                accu[i][j]+=a(i-1,j)+a(i,j-1)-a(i-1,j-1);
+        int m = matrix.size(), n = matrix[0].size();
+        sum = vector<vector<int>>(m + 1, vector<int>(n + 1)); // sum[i][j] is sum of all elements inside the rectangle [0,0,i,j]
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
     }
-    
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return a(row2,col2)-a(row1-1,col2)-a(row2,col1-1)+a(row1-1,col1-1);
-    }
-private:
-    vector<vector<int>> accu;
-    int a(int i,int j){
-        return i>=0 && j>=0? accu[i][j]:0;
+    int sumRegion(int r1, int c1, int r2, int c2) {
+		// Since our `sum` starts by 1 so we need to increase r1, c1, r2, c2 by 1
+        r1++; c1++; r2++; c2++; 
+        return sum[r2][c2] - sum[r2][c1 - 1] - sum[r1 - 1][c2] + sum[r1 - 1][c1 - 1];
     }
 };
 
