ed1cbd6a89f1e0c1798dd57d5b14251ebdc6dcba
Prantik Mukherjee <champmaniac04@gmail.com>
2022-06-05 13:37:24 +0530

    Time: 5 ms (86.06%), Space: 13.7 MB (73.29%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,35 +1,13 @@
-// // TC O(N^2) SC O(1)
-// class Solution {
-// public:
-//     int maxProduct(vector<int>& nums) {
-//         int ans = nums[0], mul;
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             mul= nums[i];
-//             ans = max(ans,mul);
-//             for(int j= i+1;j<nums.size();j++)
-//             {
-//                 mul *= nums[j];
-//                 ans= max(ans,mul);
-//             }
-//         }
-//         return ans;
-//     }
-// };
-// Space-Optimized using Dp (Tabulation)
-// TC O(N) SC O(1)
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int dpMin =nums[0],dpMax =nums[0];
-        int ans =nums[0];
-        for(int i=1;i<n;i++){
-            auto prevDpMin = dpMin, prevDpMax = dpMax;
-            dpMin=min(nums[i],nums[i]*(nums[i]>=0?prevDpMin:prevDpMax));
-            dpMax=max(nums[i],nums[i]*(nums[i]>=0?prevDpMax:prevDpMin));
-            ans=max(ans,dpMax);
+        int n = nums.size();
+        int res = nums[0], l=0,r=0;
+        for(int i=0;i<n;i++){
+            l=(l?l:1)*nums[i];
+            r=(r?r:1)*nums[n-1-i];
+            res = max(res,max(l,r));
         }
-        return ans;
+        return res;
     }
 };
