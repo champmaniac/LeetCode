de55430527f66a0ad2121c7b42cd0af0e506e427
Prantik Mukherjee <champmaniac04@gmail.com>
2022-06-13 18:24:20 +0530

    Time: 19 ms (6.24%), Space: 9.1 MB (26.05%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+    int helper(int i,int j,int n,vector<vector<int>> &triangle,vector<vector<int>> &dp){
+        if(i==n-1) return triangle[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int bottom = triangle[i][j]+helper(i+1,j,n,triangle,dp);
+        int bottom_right = triangle[i][j]+helper(i+1,j+1,n,triangle,dp);
+        return dp[i][j]=min (bottom,bottom_right);
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<int> minlen(triangle.back());
-        for(int k=n-2;k>=0;k--){ // for each layer k=layer
-            for(int i=0;i<=k;i++){
-                minlen[i]= min(minlen[i],minlen[i+1])+ triangle[k][i];
-            }
-        }
-        return minlen[0];
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        return helper(0,0,n,triangle,dp);
     }
 };
