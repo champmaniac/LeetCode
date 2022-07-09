b0ded7c4ba12d68aa417d70c78a056b3a239c0c4
Prantik Mukherjee <champmaniac04@gmail.com>
2022-07-09 14:06:57 +0530

    Time: 74 ms (29.55%), Space: 19.1 MB (66.58%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    static bool comparator( vector<int>& a, vector<int>& b ){
+        return a[0] < b[0];
+    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> res;
-        if(intervals.size()==0) return res; // base case when there are no intervals
-        sort(intervals.begin(),intervals.end()); // sort will take O(nlogn)
+        // edge cases
+        if(intervals.empty()) return res;
+        if(intervals.size()==1) {
+            res.push_back(intervals[0]);
+            return res;
+        }
+        // sort by start of intervals
+        sort(intervals.begin(),intervals.end());
         res.push_back(intervals[0]);
-        int j=0;
-        for(int i=1;i<intervals.size();i++){
-            if(res[j][1]>=intervals[i][0])
-            {
-                res[j][1]= max(res[j][1], intervals[i][1]);
-            }
-            else
-            {
-                j++;
+        int i =1;
+        while(i<intervals.size()){
+            vector<int> &last = res.back();
+            if(last[1]<intervals[i][0])
                 res.push_back(intervals[i]);
+            else{
+                last[1] = max(last[1],intervals[i][1]);
             }
+            i++;
         }
         return res;
     }
