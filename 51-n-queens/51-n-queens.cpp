d6d33484211fa9f7df33a23e69f3c221fc4c3674
Prantik Mukherjee <champmaniac04@gmail.com>
2022-07-20 14:16:13 +0530

    Time: 3 ms (96.67%), Space: 7.2 MB (70.91%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string> &board, int n){
-        // we need to check only for the left upper diagonal i.e row-- , col--
-        // left in the same row i.e col --
-        // left in the lower diagonal i.e row++ , col--
-        
-        int dupRow = row;
-        int dupCol = col;
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
-        row = dupRow;
-        col = dupCol;
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
-        row = dupRow;
-        col = dupCol;
-        while(row<n && col>=0){
-            if(board[row][col]=='Q') return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n){
+    // Optimized way using hashing --> TC O(N) SC O(N)
+    void solve(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &leftRow, vector<int> &lowerDiagonal, vector<int> &upperDiagonal, int n){
         // base 
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
                 board[row][col] = 'Q';
-                solve(col+1,board,ans,n);
+                leftRow[row] = 1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+col-row]=1;
+                solve(col+1,board,ans,leftRow,lowerDiagonal,upperDiagonal,n);
                 board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+col-row]=0;
             }
         }
     }
@@ -48,7 +28,59 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        solve(0,board,ans,n);
+        vector<int> leftRow(n,0), lowerDiagonal(2*n-1,0), upperDiagonal(2*n-1,0);
+        solve(0,board,ans,leftRow,lowerDiagonal,upperDiagonal,n);
         return ans;
     }
+    // Recursion way --> TC O(N*N) SC -> O(1)
+    // bool isSafe(int row, int col, vector<string> &board, int n){
+    //     // we need to check only for the left upper diagonal i.e row-- , col--
+    //     int dupRow = row;
+    //     int dupCol = col;
+    //     while(row>=0 && col>=0){
+    //         if(board[row][col]=='Q') return false;
+    //         row--;
+    //         col--;
+    //     }
+    //     // left in the same row i.e col --
+    //     row = dupRow;
+    //     col = dupCol;
+    //     while(col>=0){
+    //         if(board[row][col]=='Q') return false;
+    //         col--;
+    //     }
+    //     // left in the lower diagonal i.e row++ , col--
+    //     row = dupRow;
+    //     col = dupCol;
+    //     while(row<n && col>=0){
+    //         if(board[row][col]=='Q') return false;
+    //         row++;
+    //         col--;
+    //     }
+    //     return true;
+    // }
+    // void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n){
+    //     // base 
+    //     if(col==n){
+    //         ans.push_back(board);
+    //         return;
+    //     }
+    //     for(int row=0;row<n;row++){
+    //         if(isSafe(row,col,board,n)){
+    //             board[row][col] = 'Q';
+    //             solve(col+1,board,ans,n);
+    //             board[row][col] = '.';
+    //         }
+    //     }
+    // }
+    // vector<vector<string>> solveNQueens(int n) {
+    //     vector<vector<string>> ans;
+    //     vector<string> board(n);
+    //     string s(n,'.');
+    //     for(int i=0;i<n;i++){
+    //         board[i]=s;
+    //     }
+    //     solve(0,board,ans,n);
+    //     return ans;
+    // }
 };
