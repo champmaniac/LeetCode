14d47bc454fa87fb10e0990b59057f189f3cc342
Prantik Mukherjee <champmaniac04@gmail.com>
2022-07-20 14:04:14 +0530

    Time: 9 ms (64.07%), Space: 7.2 MB (87.97%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string> board, int n){
-        // check the upper diagonal
-        int duprow = row;
-        int dupcol = col;
+    bool isSafe(int row, int col, vector<string> &board, int n){
+        // we need to check only for the left upper diagonal i.e row-- , col--
+        // left in the same row i.e col --
+        // left in the lower diagonal i.e row++ , col--
+        
+        int dupRow = row;
+        int dupCol = col;
         while(row>=0 && col>=0){
             if(board[row][col]=='Q') return false;
             row--;
             col--;
         }
-        
-        col = dupcol;
-        row = duprow;
+        row = dupRow;
+        col = dupCol;
         while(col>=0){
             if(board[row][col]=='Q') return false;
             col--;
         }
-        
-        row = duprow;
-        col = dupcol;
-        
+        row = dupRow;
+        col = dupCol;
         while(row<n && col>=0){
             if(board[row][col]=='Q') return false;
             row++;
@@ -27,20 +27,20 @@ public:
         }
         return true;
     }
-    void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
+    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n){
+        // base 
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
             if(isSafe(row,col,board,n)){
-                board[row][col]='Q';
+                board[row][col] = 'Q';
                 solve(col+1,board,ans,n);
-                board[row][col]='.';
+                board[row][col] = '.';
             }
         }
     }
-public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
