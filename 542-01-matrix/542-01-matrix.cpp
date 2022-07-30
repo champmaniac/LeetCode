7c451330c9293309fff747e6b6b8450ef262de14
Prantik Mukherjee <champmaniac04@gmail.com>
2022-07-30 15:18:45 +0530

    Time: 208 ms (14.43%), Space: 30.4 MB (39.05%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
-    bool isValid(int i,int j,int m,int n){
+    bool isvalid(int i,int j,int m,int n){
         if(i<0 || j<0 || i>=m || j>=n) return false;
         return true;
     }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<int>> vis(m,vector<int> (n,-1));
-        queue<pair<int,int>> q;
         
-        vector<vector<int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+        vector<vector<int>> ans(m,vector<int>(n,0)); // ans cum visited array
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
+                    ans[i][j]=0;
                     q.push({i,j});
-                    vis[i][j]=0;
+                }
+                else{
+                    ans[i][j]=-1;
                 }
             }
         }
+        vector<vector<int>> dir ={{1,0},{0,1},{-1,0},{0,-1}};
+        int level=0;
         while(!q.empty()){
-            pair<int,int> top = q.front();
-            q.pop();
-            for(auto &it:dir){
-                int x = top.first+it[0];
-                int y = top.second+it[1];
-                
-                if(isValid(x,y,m,n) && vis[x][y]==-1){
-                    q.push({x,y});
-                    vis[x][y]=vis[top.first][top.second]+1;
+            int size = q.size();
+            while(size--){
+                pair<int,int> p = q.front();
+                q.pop();
+                int r = p.first;
+                int c = p.second;
+                for(int i=0;i<4;i++){
+                    int nr = r+dir[i][0];
+                    int nc = c+dir[i][1];
+                    
+                    if(isvalid(nr,nc,m,n) && ans[nr][nc]==-1){
+                        ans[nr][nc]=level+1;
+                        q.push({nr,nc});
+                    }
                 }
             }
+            level++;
         }
-        return vis;
+        return ans;
     }
 };
