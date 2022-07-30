553898a6fac22b1b6aad4ec03e8be72dd771ac21
Prantik Mukherjee <champmaniac04@gmail.com>
2022-07-30 11:39:58 +0530

    Time: 11 ms (62.01%), Space: 12.9 MB (94.37%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
-    int dx[4] = {0,0,-1,1};
-    int dy[4] = {1,-1,0,0};
-    bool isvalid(int x,int y,int m,int n){
-        if(x<0 || x>=m || y<0 || y>=n) return false;
+    bool isvalid(int i,int j,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n) return false;
         return true;
     }
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
-        int n =grid[0].size();
-        queue<pair<int,int>> rotten;
-        int mini=0,tot=0,cnt=0;
+        int n = grid[0].size();
+        
+        queue<pair<int,int>> q;
+        
+        int minutes=0,tot=0,count=0;
         if(grid.empty()) return 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]!=0) tot++;
-                if(grid[i][j]==2) rotten.push({i,j});
-            }
-        }
-        
-        while(!rotten.empty()){
-            int sz = rotten.size();
-            cnt+=sz;
-            while(sz--){
-                int x = rotten.front().first, y = rotten.front().second;
-                rotten.pop();
-                for(int i=0;i<4;i++){
-                    int x1 = x+dx[i];
-                    int y1 = y+dy[i];
-                    if(!isvalid(x1,y1,m,n) || grid[x1][y1]!=1) continue;
-                    grid[x1][y1]=2;
-                    rotten.push({x1,y1});
+                if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
-            if(!rotten.empty()) mini++;
         }
-        return tot==cnt?mini:-1;
+        int di[4] = {0,1,0,-1};
+        int dj[4] = {1,0,-1,0};
+        
+        while(!q.empty()){
+            int size=q.size();
+            count+=size;
+            while(size--){
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int dx = x+di[i];
+                    int dy = y+dj[i];
+                    
+                    if(isvalid(dx,dy,m,n) && grid[dx][dy]==1){
+                        grid[dx][dy]=2;
+                        q.push({dx,dy});
+                    }
+                }
+            }
+            if(!q.empty()) minutes++;
+        }
+        return count==tot?minutes:-1;
     }
 };
