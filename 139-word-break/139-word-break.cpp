0f23574c8294f5e2292b4f9bd30852c63a828e37
Prantik Mukherjee <champmaniac04@gmail.com>
2022-08-03 23:14:48 +0530

    Time: 30 ms (36.38%), Space: 9.4 MB (67.27%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool helper(int ind, string s, set<string>&st,vector<int> &dp){
-        if(ind==s.size()) return 1;
-        if(dp[ind]!=-1) return dp[ind];
+    int helper(int i,string s,set<string>&wordDict,vector<int> &dp){
+        int n = s.size();
+        if(i==n) return 1;
+        if(dp[i]!=-1) return dp[i];
         string temp;
-        for(int i=ind;i<s.size();i++){
-            temp+=s[i];
-            if(st.find(temp)!=st.end()){
-                if(helper(i+1,s,st,dp)) 
-                    return dp[ind]=1;
+        for(int j=i;j<n;j++){
+            temp+=s[j];
+            if(wordDict.find(temp)!=wordDict.end()){
+                if(helper(j+1,s,wordDict,dp)) return dp[i]= 1;
             }
         }
-        return dp[ind]=0;
+        return dp[i]=0;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
         set<string> st;
         for(auto it:wordDict){
             st.insert(it);
         }
-        vector<int> dp(n+1,-1);
+        vector<int> dp(s.size()+1,-1);
         return helper(0,s,st,dp);
     }
 };
