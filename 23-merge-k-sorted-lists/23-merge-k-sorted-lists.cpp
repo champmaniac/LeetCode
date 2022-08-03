dd68eb13cb112272ec34e57a427b7c64081c3c7d
Prantik Mukherjee <champmaniac04@gmail.com>
2022-08-03 15:48:24 +0530

    Time: 265 ms (20.67%), Space: 13.4 MB (47.77%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,25 +10,26 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* l1, ListNode* l2){
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if(l1==NULL) return l2;
         if(l2==NULL) return l1;
-        if(l1->val<l2->val){
-            l1->next = merge(l1->next,l2);
+        
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
             return l1;
         }
         else{
-            l2->next = merge(l1,l2->next);
+            l2->next=mergeTwoLists(l1,l2->next);
             return l2;
         }
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.empty())  
             return NULL ; 
-        int ind = lists.size()-1;
-        while(ind>0){
-            lists[ind-1] = merge(lists[ind],lists[ind-1]);
-            ind--;
+        int k = lists.size()-1;
+        while(k>0){
+            lists[k-1] = mergeTwoLists(lists[k],lists[k-1]);
+            k--;
         }
         return lists[0];
     }
