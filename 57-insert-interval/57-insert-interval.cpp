5b06201c13953e28b09acbe84c9c9ca98c14f102
Prantik Mukherjee <champmaniac04@gmail.com>
2022-08-09 17:18:34 +0530

    Time: 39 ms (20.23%), Space: 18.6 MB (5.30%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> res;
-        int size = intervals.size();
-        int i=0;
-        // no overlapping between the ending point of interval to the starting point of the newInterval
-        while(i<size && intervals[i][1]<newInterval[0]){
-            res.push_back(intervals[i]);
-            i++;
+        vector<vector<int>> copyIntervals;
+        int n = intervals.size();
+        for(int i=0;i<n;i++){
+            copyIntervals.push_back(intervals[i]);
         }
-        // overlapping case and merge intervals
-        while(i<size && newInterval[1]>=intervals[i][0]){
-            newInterval[0] = min(newInterval[0],intervals[i][0]);
-            newInterval[1] = max(newInterval[1],intervals[i][1]);
-            i++;
+        copyIntervals.push_back(newInterval);
+        sort(copyIntervals.begin(),copyIntervals.end());
+        vector<vector<int>> merged;
+        int size = copyIntervals.size();
+        for(int i=0;i<size;i++){
+            if(merged.empty() || merged.back()[1]<copyIntervals[i][0]){
+                vector<int> v = {copyIntervals[i][0],copyIntervals[i][1]};
+                merged.push_back(v);
+            }
+            else{
+                merged.back()[1] = max(merged.back()[1],copyIntervals[i][1]);
+            }
         }
-        res.push_back(newInterval);
-        // no overlapping after newInterval being merged
-        while(i<size){
-            res.push_back(intervals[i]);
-            i++;
-        }
-        return res;
+        return merged;
     }
 };
