afac021590f2afa8046c44d078a144c8f78e7c13
Prantik Mukherjee <champmaniac04@gmail.com>
2022-08-30 00:52:14 +0530

    Time: 0 ms (100.00%), Space: 6 MB (51.53%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        int fact =1;
         vector<int> numbers;
+        // calculating the factorial and all the numbers from 1-n in numbers array
+        int fact=1;
         for(int i=1;i<n;i++){
             fact=fact*i;
             numbers.push_back(i);
         }
         numbers.push_back(n);
-        string ans = "";
-        k=k-1; // 0 based indexing
+        k = k -1;
+        string ans="";
         while(true){
-            ans = ans+to_string(numbers[k/fact]);
+            ans=ans+to_string(numbers[k/fact]);
             numbers.erase(numbers.begin()+k/fact);
             if(numbers.size()==0) break;
-            k =k%fact;
-            fact = fact/numbers.size();
+            k=k%fact;
+            fact=fact/numbers.size();
         }
         return ans;
     }
