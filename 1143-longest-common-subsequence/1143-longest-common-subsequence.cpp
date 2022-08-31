ea7e05358bcfcf2bd53ab5245a9b6c6c54204598
Prantik Mukherjee <champmaniac04@gmail.com>
2022-08-31 16:35:54 +0530

    Time: 11 ms (95.82%), Space: 10.6 MB (78.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    int LcsHelper(string X, string Y,int m,int n){
-        int dp[m+1][n+1];
-        for(int i=0;i<m+1;i++){
-            for(int j=0;j<n+1;j++){
+    int lcsHelper(string text1,string text2,int n,int m){
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
                 if(i==0 || j==0) dp[i][j]=0;
             }
         }
-        
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
-                if(X[i-1]==Y[j-1]) dp[i][j]= 1+dp[i-1][j-1];
-                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+                }
             }
         }
-        return dp[m][n];
+        return dp[n][m];
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size(), n = text2.size();
-        return LcsHelper(text1,text2,m,n);
+        int n = text1.size();
+        int m = text2.size();
+        return lcsHelper(text1,text2,n,m);
     }
 };
