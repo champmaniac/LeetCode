da6c2e27bda8ca42bbac051ccc167920cf503571
Prantik Mukherjee <champmaniac04@gmail.com>
2022-09-11 08:10:03 +0530

    Time: 200 ms (18.49%), Space: 36.9 MB (34.51%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
+    const int mod = (int)1e9+7;
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        int mod = (int)1e9+7;
         // to maintain the top k-1 engineers
         priority_queue<int,vector<int>,greater<int>> pq;
-        
-        long long ans=0;
-        
         vector<pair<int,int>> v;
         for(int i=0;i<n;i++){
             v.push_back({efficiency[i],speed[i]});
         }
+        // sorting in decreasing order.
         sort(v.rbegin(),v.rend());
-        long long topSum=0;
+        long totSpeed=0,ans=0;
         for(int i=0;i<n;i++){
-            int cur_min = v[i].first;
-            long long speedSum = 1LL*v[i].second+topSum;
-            ans = max(ans,1LL*speedSum*cur_min);
-            pq.push(v[i].second);
-            
-            
-            topSum+=1LL*v[i].second;
-            if(pq.size()>k-1){
-                topSum-=1LL*pq.top();
+            int curSpeed = v[i].second;
+            totSpeed+=curSpeed;
+            pq.push(curSpeed);
+            if(pq.size()>k){
+                totSpeed-=pq.top();
                 pq.pop();
             }
+            int minEfficiency = v[i].first;
+            ans = max(ans,totSpeed*minEfficiency);
         }
-        ans%=mod;
-        return ans;
+        return ans%mod;
     }
 };
