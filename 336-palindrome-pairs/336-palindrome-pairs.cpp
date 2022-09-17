b363a13e7ab795e8542951198fd6a78b0df720d4
Prantik Mukherjee <champmaniac04@gmail.com>
2022-09-17 11:29:00 +0530

    Time: 1698 ms (51.57%), Space: 594.1 MB (19.48%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -1,53 +1,65 @@
-struct TrieNode {
-    TrieNode *next[26] = {};
-    int index = -1;
-    vector<int> palindromeIndexes;
+struct TrieNode
+{
+    TrieNode *child[26]={};
+    int curIndex=-1;
+    vector<int> wordIndex;
 };
-class Solution {
-    bool isPalindrome(string &s, int i, int j) {
-        while (i < j && s[i] == s[j]) ++i, --j;
-        return i >= j;
+class Solution
+{
+    bool isPalindrome(string &s, int i, int j)
+    {
+        while (i < j)
+        {
+            if (s[i++] != s[j--])
+                return false;
+        }
+        return true;
     }
     TrieNode *root;
-    void insert(string &s, int index) {
-        TrieNode *node = root;
-        for (int i = s.size() - 1; i >= 0; i--) {
+    void insert(string &s, int index)
+    {
+        TrieNode *cur = root;
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
             int c = s[i] - 'a';
-            if (!node->next[c]) {
-                node->next[c] = new TrieNode();
-            }
-            if (isPalindrome(s, 0, i)) {
-                node->palindromeIndexes.push_back(index);
-            }
-            node = node->next[c];
+            if (cur->child[c] == nullptr)
+                cur->child[c] = new TrieNode();
+            if (isPalindrome(s, 0, i))
+                cur->wordIndex.push_back(index);
+            cur = cur->child[c];
         }
-        node->index = index;
-        node->palindromeIndexes.push_back(index);
+        cur->wordIndex.push_back(index);
+        cur->curIndex = index;
     }
-    public:
-    vector<vector<int>> palindromePairs(vector<string>& words) {
+
+public:
+    vector<vector<int>> palindromePairs(vector<string> &words)
+    {
         root = new TrieNode();
-        for (int i = 0; i < words.size(); i++) {
+        for (int i = 0; i < words.size(); i++)
             insert(words[i], i);
-        }
-        vector<vector<int>> result;
-        for (int i = 0; i < words.size(); i++) {
-            TrieNode *node = root;
+        vector<vector<int>> ans;
+        for (int i = 0; i < words.size(); i++)
+        {
+            TrieNode *cur = root;
             string &s = words[i];
-            for (int j = 0; j < s.size(); j++) {
-                if (node->index >= 0 && node->index != i && isPalindrome(s, j, s.size() - 1)) {
-                    result.push_back({i, node->index});
-                }
-                node = node->next[s[j] - 'a'];
-                if (!node) break;
+            for (int j = 0; j < s.size(); j++)
+            {
+                if (cur->curIndex != -1 && cur->curIndex != i && isPalindrome(s, j, s.size() - 1))
+                    ans.push_back({i, cur->curIndex});
+                cur = cur->child[s[j] - 'a'];
+                if (cur == nullptr)
+                    break;
             }
-            if (node) {
-                for (int j : node->palindromeIndexes) {
-                    if (i == j) continue;
-                    result.push_back({i, j});
-                }
+            if (cur == nullptr)
+                continue;
+            for (int j : cur->wordIndex)
+            {
+                if (i == j)
+                    continue;
+                ans.push_back({i, j});
             }
         }
-        return result;
+        return ans;
     }
 };
