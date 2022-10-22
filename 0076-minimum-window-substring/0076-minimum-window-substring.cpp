a933539a1c33da11bad3165286054dc7431c3beb
Prantik Mukherjee <champmaniac04@gmail.com>
2022-10-22 09:57:26 +0530

    Time: 17 ms (83.57%), Space: 7.7 MB (68.10%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size(), m = t.size();
+        unordered_map<char,int> letters;
+        for(auto c:t){
+            letters[c]++;
+        }
+        int count=0;
+        int low=0,min_length=INT_MAX, min_start=0;
+        for(int high=0;high<n;high++){
+            if(letters[s[high]]>0) count++;
+            letters[s[high]]--;
+            if(count==m){
+                while(low<high && letters[s[low]]<0)
+                    letters[s[low]]++, low++;
+                if(min_length>high-low){
+                    min_length = high - (min_start=low)+1;
+                }
+                letters[s[low++]]++;
+                count--;
+            }
+        }
+        return min_length == INT_MAX? "":s.substr(min_start,min_length);
+        
+    }
+};
