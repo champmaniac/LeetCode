5c5390a97883f54538c6e9a0cb32a41feb798018
Prantik Mukherjee <champmaniac04@gmail.com>
2023-01-09 16:58:49 +0530

    Time: 12 ms (81.60%), Space: 8.9 MB (61.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -11,10 +11,26 @@ public:
             return dp[i][j]=1+min(helper(i-1,j,s,t,dp),min(helper(i-1,j-1,s,t,dp),helper(i,j-1,s,t,dp)));
         }
     }
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return helper(n,m,word1,word2,dp);
+    int minDistance(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
